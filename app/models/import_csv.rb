e5d696dfc5171610fac998f990408070e2b2c900
akiyoshi_okumura <akiyoshia.m.1231@gmail.com>
2019-09-23 17:06:06 +0900

    add csv import

---
+++ app/models/import_csv.rb
@@ -0,0 +1,25 @@
+class ImportCsv < ApplicationRecord
+  # CSVデータのパスを引数として受け取り、インポート処理を実行
+  def self.import(path)
+    #インポートするファイルのパスを取得
+    path = File.join Rials.root, "db/csv/csv_data.csv"
+    #インポートするデータを格納するための配列
+    list = []
+    #CSVファイルからインポートするデータを取得し配列に格納
+     CSV.foreach(path, headers: true) do |row|
+       list << (
+         name: row["name"],
+         age: row["age"],
+         address: row["address"]
+       )
+     end
+     puts "インポート処理を開始"
+     #インポートができなかった場合の例外処理
+     begin
+       User.create!(list)
+       puts "インポート完了"
+     rescue ActiveModel::UnknownAttributeError => invalid
+        puts "インポートに失敗:UnknownAttributeError" 
+     end
+  end
+end
